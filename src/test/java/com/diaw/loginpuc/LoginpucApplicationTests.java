ca4caaabbe4aa996f53773e39f7e72d074d1022a
joao.reis <joao.reis@xpe.edu.br>
2026-09-14 19:46:53 -0300

    feat: content

---
+++ src/test/java/com/diaw/loginpuc/LoginpucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diaw.loginpuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginpucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
